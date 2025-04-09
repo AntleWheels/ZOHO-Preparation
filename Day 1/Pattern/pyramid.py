86368adc293c03e16b50a2849cb07f79338a417a
Guru Rahul <100460204+AntleWheels@users.noreply.github.com>
2025-04-09 11:51:54 +0530

    Update pyramid.py

--- Day 1/Pattern/pyramid.py
+++ Day 1/Pattern/pyramid.py
@@ -4,3 +4,7 @@ def logical_phase(n):
 if __name__ =="__main__":
     height = int(input("Enter the height of the triangle :"))
     logical_phase(height)
+
+
+#Add the space first and then add the *
+#gradually decrease the space and increase the *
